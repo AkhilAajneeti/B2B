d0ff20a7a57958c6ab59f8e513f58fe0cf73a8d9
AkhilAajneeti <akhilaajneeti@gmail.com>
2026-07-23 12:26:41 +0530

    update system

--- src/components/RoleGuard.jsx
+++ src/components/RoleGuard.jsx
@@ -2,7 +2,7 @@ const RoleGuard = ({ allowedRoles = [], children }) => {
   const storedUser = localStorage.getItem("login_object");
 
   if (!storedUser) return null;
-
+ 
   const user = JSON.parse(storedUser);
 
   const userType = user?.type;
